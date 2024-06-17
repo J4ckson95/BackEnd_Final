3e968fdc92b89039f4c97adce30ea5d008267782
Jason López <j4ckson95@gmail.com>
2024-06-17 07:59:26 -0500

    pasarela de pago terminada

--- src/Controller/stripePayment.controller.js
+++ src/Controller/stripePayment.controller.js
@@ -1,8 +1,16 @@
 import config from "../Config/config.js";
 import stripePackage from "stripe"
-export const PaymentIntent = (req, res) => {
-    const { payload } = req.body
-    const stripe = stripePackage(config.StripeSecrectKey);
-    console.log(payload);
 
+const stripe = stripePackage(config.StripeSecrectKey);
+
+export const PaymentIntent = async (req, res) => {
+    const { totalPrice } = req.body
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: totalPrice,
+        currency: "usd",
+        automatic_payment_methods: {
+            enabled: true
+        }
+    })
+    res.send({ clientSecret: paymentIntent.client_secret })
 }
--- src/utils.js
+++ src/utils.js
@@ -2,11 +2,11 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import config from "./Config/config.js"
 
-export const createHas = (password) => {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+export const createHas = async (password) => {
+    return await bcrypt.hash(password, bcrypt.genSaltSync(10))
 }
-export const validateHas = (password, user) => {
-    return bcrypt.compareSync(password, user.password)
+export const validateHas = async (password, user) => {
+    return bcrypt.compare(password, user.password)
 }
 export const generateToken = (user) => {
     const token = jwt.sign({ user }, config.PRIVATE_KEY, { expiresIn: "24h" })
